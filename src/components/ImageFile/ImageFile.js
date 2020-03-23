6ef80b022374389216d68b0a4a3df200bb3b58c1
Aadarsh chhetry <41640633+aadarshchhetry@users.noreply.github.com>
2020-03-23 14:06:03 +0530

    image css

--- src/components/ImageFile/ImageFile.js
+++ src/components/ImageFile/ImageFile.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './ImageFile.css'
+
 const ImageFile = () => {
   return (
         <img src="https://www.linkedin.com/dms/C5104AQEoD0STKurWhw/profile-originalphoto-shrink_900_1200/0?m=AQJ5jJ9KOHPcEwAAAXEGc6aT3v1GSo-I9XEThliE3xXsvxQUnfHFGTJfeQ&e=1585037574&v=beta&t=xrVGTBI2IiAuZgOEqoz3WHMyHBdb89iNX5RMZTa6bPg" />
