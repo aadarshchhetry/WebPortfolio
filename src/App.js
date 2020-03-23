828e4f1b6b60217aa89b955a0991ba38f74c7c2a
Aadarsh chhetry <41640633+aadarshchhetry@users.noreply.github.com>
2020-03-23 13:40:22 +0530

    created Image component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ImageFile from './Components/ImageFile/ImageFile'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ImageFile />
         
       </header>
     </div>
