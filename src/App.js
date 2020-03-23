266ba9671ec75fb28627a1ddb7693603bc4f5576
Aadarsh chhetry <41640633+aadarshchhetry@users.noreply.github.com>
2020-03-23 13:56:24 +0530

    added Image

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import ImageFile from './Components/ImageFile/ImageFile'
+import ImageFile from './components/ImageFile/ImageFile'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <ImageFile />
-        
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor() {
+    super()
+  }
+  render(){
+      return (
+        <div className="App">
+          <header className="App-header">
+            < ImageFile />
+            
+          </header>
+        </div>
+      );
+    }
 }
 
 export default App;
---
+++ src/components/ImageFile/ImageFile.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const ImageFile = () => {
+  return (
+        <img src="https://www.linkedin.com/dms/C5104AQEoD0STKurWhw/profile-originalphoto-shrink_900_1200/0?m=AQJ5jJ9KOHPcEwAAAXEGc6aT3v1GSo-I9XEThliE3xXsvxQUnfHFGTJfeQ&e=1585037574&v=beta&t=xrVGTBI2IiAuZgOEqoz3WHMyHBdb89iNX5RMZTa6bPg" />
+  );
+}
+
+export default ImageFile;
