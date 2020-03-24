816287572cd393950e9b1fa9ac815c4726e15636
Aadarsh chhetry <41640633+aadarshchhetry@users.noreply.github.com>
2020-03-24 12:49:53 +0530

    added image to file

--- src/components/ImageFile/ImageFile.js
+++ src/components/ImageFile/ImageFile.js
@@ -4,7 +4,7 @@ import './ImageFile.css'
 const ImageFile = () => {
   return (
     <div>
-        <img alt="" src=".\download-min.png" />
+        <img alt="" src="download-min.png" />
         <h1>AADARSH CHHETRY</h1>
         <h5>“The first step is to establish that something is possible; then probability will occur.” ― Elon Musk</h5>
     </div>
