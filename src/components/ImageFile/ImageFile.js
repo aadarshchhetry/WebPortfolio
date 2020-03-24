e0c6d25ae1665228cd6843b7e21ea24fda689a8c
Aadarsh chhetry <41640633+aadarshchhetry@users.noreply.github.com>
2020-03-24 13:34:38 +0530

    Revert "added image to file"
    This reverts commit 39a1b0dd3efa562fbd3b85dd94db1fbae66b2230.

--- src/components/ImageFile/ImageFile.js
+++ src/components/ImageFile/ImageFile.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './ImageFile.css';
-import Logo from './download-min.png';
+import './ImageFile.css'
 
 const ImageFile = () => {
   return (
     <div>
-        <img alt="" src={Logo} />
+        <img alt="" src="download-min.png" />
         <h1>AADARSH CHHETRY</h1>
         <h5>“The first step is to establish that something is possible; then probability will occur.” ― Elon Musk</h5>
     </div>
