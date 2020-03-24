6b36e62fda5df02a0617f02d3f578b0035fb41d2
Aadarsh chhetry <41640633+aadarshchhetry@users.noreply.github.com>
2020-03-24 12:39:12 +0530

    added image to file

--- src/components/ImageFile/ImageFile.js
+++ src/components/ImageFile/ImageFile.js
@@ -4,9 +4,9 @@ import './ImageFile.css'
 const ImageFile = () => {
   return (
     <div>
-        <img alt="" src="https://www.linkedin.com/dms/C5104AQEoD0STKurWhw/profile-originalphoto-shrink_900_1200/0?m=AQJ5jJ9KOHPcEwAAAXEGc6aT3v1GSo-I9XEThliE3xXsvxQUnfHFGTJfeQ&e=1585037574&v=beta&t=xrVGTBI2IiAuZgOEqoz3WHMyHBdb89iNX5RMZTa6bPg" />
+        <img alt="" src="download-min.png" />
         <h1>AADARSH CHHETRY</h1>
-        <h4>“The first step is to establish that something is possible; then probability will occur.” ― Elon Musk</h4>
+        <h5>“The first step is to establish that something is possible; then probability will occur.” ― Elon Musk</h5>
     </div>
   );
 }
