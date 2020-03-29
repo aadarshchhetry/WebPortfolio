642efa39406c9d722dc6ff663b36d393f8b22753
Aadarsh chhetry <41640633+aadarshchhetry@users.noreply.github.com>
2020-03-29 23:58:26 +0530

    min box height

--- src/components/ListFile/List.js
+++ src/components/ListFile/List.js
@@ -11,7 +11,7 @@ const List = () => {
                     <div class="col-md-4">
                     <div class="card mb-4 shadow-sm">
                     <a href="https://www.linkedin.com/in/aadarsh-chhetry-54ba4a166/">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><rect width="100%" height="100%" fill="#0072b1"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">LinkedIn</text></svg> 
+                        <svg class="bd-placeholder-img card-img-top" width="90%" height="50" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><rect width="90%" height="100%" fill="#0072b1"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">LinkedIn</text></svg> 
                     </a>                   
                     </div>
                     </div>
@@ -20,14 +20,14 @@ const List = () => {
                 <div class="col-md-4">
                 <div class="card mb-4 shadow-sm">
                 <a href="https://github.com/aadarshchhetry">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><rect width="100%" height="100%" fill="#211F1F"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">GitHub</text></svg>                    
+                    <svg class="bd-placeholder-img card-img-top" width="90%" height="50" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><rect width="90%" height="100%" fill="#211F1F"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">GitHub</text></svg>                    
                     </a></div>
                 </div>
 
                 <div class="col-md-4">
                 <div class="card mb-4 shadow-sm">
                 <a href="https://www.instagram.com/aaadarshchhetry/?hl=en">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><rect width="100%" height="100%" fill="#f2003c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Instagram</text></svg>                    
+                    <svg class="bd-placeholder-img card-img-top" width="90%" height="50" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><rect width="90%" height="100%" fill="#f2003c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Instagram</text></svg>                    
                     </a></div>
                 </div>
 
@@ -36,21 +36,21 @@ const List = () => {
                 <div class="col-md-4">
                 <div class="card mb-4 shadow-sm">
                 <a href="https://facerecognition1100.herokuapp.com/">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><rect width="100%" height="100%" fill="#800000"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Face Recognition App</text></svg>                    
+                    <svg class="bd-placeholder-img card-img-top" width="90%" height="50" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><rect width="90%" height="100%" fill="#800000"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Face Recognition App</text></svg>                    
                     </a></div>
                 </div>
 
                 <div class="col-md-4">
                 <div class="card mb-4 shadow-sm">
                 <a href="#">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><rect width="100%" height="100%" fill="#3b5998"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Facebook</text></svg>                    
+                    <svg class="bd-placeholder-img card-img-top" width="90%" height="50" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><rect width="90%" height="100%" fill="#3b5998"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Facebook</text></svg>                    
                     </a></div>
                 </div>
 
                 <div class="col-md-4">
                 <div class="card mb-4 shadow-sm">
                 <a href="#">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Freelancer</text></svg>                    
+                    <svg class="bd-placeholder-img card-img-top" width="90%" height="50" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><rect width="90%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Freelancer</text></svg>                    
                 </a></div>
                 </div>
 
